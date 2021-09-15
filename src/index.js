7ea8c587ffa7732050720cc24219a845ba6eee39
FedericoBlanco <blancofedem@gmail.com>
2021-09-14 22:09:13 -0300

    Cambio de dos renders a uno solo que englobe App.js y NavBar.js

--- src/index.js
+++ src/index.js
@@ -9,12 +9,6 @@ import NavBar from "./components/NavBar";
 ReactDOM.render(
   <React.StrictMode>
     <NavBar />
-  </React.StrictMode>,
-  document.getElementById("barraNavegacion")
-);
-
-ReactDOM.render(
-  <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
