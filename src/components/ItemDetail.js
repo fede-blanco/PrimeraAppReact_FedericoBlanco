1c12af9c77287185e85a69f0a5812d22e14305e2
FedericoBlanco <blancofedem@gmail.com>
2021-10-03 20:25:14 -0300

    Agregado del componente ItemDetailContainer.js e ItemDetail.js. Tambien se agrego la funcion de que se pueda cerrar el detalle y se vuelva a renderizar la vista de la lista de los items disponibles

---
+++ src/components/ItemDetail.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ItemCount from "./ItemCount";
+
+const ItemDetail = (props) => {
+  const { id, title, description, price, pictureUrl, selectItem } = props;
+
+  //dentro de las props se trajo  a "selectItem" para poder asociar a una variable "closeDetail" una funcion que lo vuelva "null" y haga que se renderize nuevamente la lista de items en vez del detalle
+  const closeDetail = () => selectItem(null);
+
+  //retorna una tarjeta con todos los datos del item seleccionado
+  return (
+    <div className="row">
+      <div
+        id={id}
+        className="card border-dark text-center m-auto "
+        style={{ width: "30rem" }}
+      >
+        <img
+          src={pictureUrl}
+          className="card-img-top p-1 m-auto"
+          style={{ height: "32rem", width: "18rem" }}
+          alt="..."
+        />
+
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+        </div>
+
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <strong>${price}</strong>
+          </li>
+          <li className="list-group-item">
+            <ItemCount stock="10" initial="1" />
+          </li>
+        </ul>
+
+        <div>
+          <button onClick={closeDetail} className="btn btn-danger w-50 p-1 m-2">
+            Cerrar Detalle
+          </button>
+        </div>
+      </div>
+    </div>
+    // <div>
+    //     Item Detail
+    //     <h1>Id: {id}</h1>
+    //     <h1>Title: {title}</h1>
+    //     <h1>Description: {description}</h1>
+    //     <h1>Price: {price}</h1>
+    //     <img src={pictureUrl} />
+    //     <a onClick={closeDetail}>cerrar producto</a>
+    // </div>
+
+    // <div className="modal" tabindex="-1">
+    //     <div id={id} className="modal-dialog">
+    //         <div className="modal-content">
+
+    //         <div className="modal-header">
+    //             <h1 className="modal-title">{title}</h1>
+    //             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    //         </div>
+
+    //         <div className="modal-body">
+    //             <img src={pictureUrl} />
+    //             <p>{description}</p>
+    //             <p><strong>{price}</strong></p>
+    //         </div>
+
+    //         <div className="modal-footer">
+    //             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={closeDetail}>Close</button>
+    //         </div>
+    //         </div>
+    //     </div>
+    // </div>
+  );
+};
+
+export default ItemDetail;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,63 +1,113 @@
-import React, {useState, useEffect} from "react";
-import ItemCount from "./ItemCount";
+import React, { useState, useEffect } from "react";
 import ItemList from "./ItemList";
+import ItemDetailContainer from "./ItemDetailContainer";
 
-const ItemListContainer = (props) => {
-  const { text } = props;
-
+const ItemListContainer = ({ text }) => {
   //Se inicializan los estados de "items", "loading" y "error"
-  const [items, setItems] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null);
 
   // Array con objetos producto para luego realizar los itmes para el desafio de la clase 6
   const serverData = [
-    { id: 0, title: "Guitarra Criolla Clasica Estudio Natural Bamboo", description: "Guitarra criolla", price: 9990, pictureUrl: "https://http2.mlstatic.com/D_NQ_NP_911089-MLA44273239284_122020-O.webp" },
-    { id: 1, title: "Guitarra Electrica Ibanez Gio Grx40", description: "Guitarra electrica", price: 37000, pictureUrl: "https://http2.mlstatic.com/D_NQ_NP_633887-MLA43145580826_082020-O.webp" },
-    { id: 2, title: "Squier Guitarra Eléctrica Affinity Stratocaster", description: "Guitarra electrica", price: 70000, pictureUrl: "https://http2.mlstatic.com/D_NQ_NP_702019-MLA42319037133_062020-O.webp" },
-    { id: 3, title: "Guitarra Electroacústica Fender Concert", description: "Guitarra electroacustica", price: 75000, pictureUrl: "https://http2.mlstatic.com/D_NQ_NP_675841-MLA43054184144_082020-O.webp" },
-    { id: 4, title: "Guitarra Eléctrica Les Paul Onas Frizz", description: "Guitarra electrica", price: 28900, pictureUrl: "https://http2.mlstatic.com/D_NQ_NP_885128-MLA32880469711_112019-O.webp" }
-  ]
+    {
+      id: 0,
+      title: "Guitarra Criolla Clasica Estudio Natural Bamboo",
+      description: "Guitarra criolla",
+      price: 9990,
+      pictureUrl:
+        "https://http2.mlstatic.com/D_NQ_NP_911089-MLA44273239284_122020-O.webp",
+    },
+    {
+      id: 1,
+      title: "Guitarra Electrica Ibanez Gio Grx40",
+      description: "Guitarra electrica",
+      price: 37000,
+      pictureUrl:
+        "https://http2.mlstatic.com/D_NQ_NP_633887-MLA43145580826_082020-O.webp",
+    },
+    {
+      id: 2,
+      title: "Squier Guitarra Eléctrica Affinity Stratocaster",
+      description: "Guitarra electrica",
+      price: 70000,
+      pictureUrl:
+        "https://http2.mlstatic.com/D_NQ_NP_702019-MLA42319037133_062020-O.webp",
+    },
+    {
+      id: 3,
+      title: "Guitarra Electroacústica Fender Concert",
+      description: "Guitarra electroacustica",
+      price: 75000,
+      pictureUrl:
+        "https://http2.mlstatic.com/D_NQ_NP_675841-MLA43054184144_082020-O.webp",
+    },
+    {
+      id: 4,
+      title: "Guitarra Eléctrica Les Paul Onas Frizz",
+      description: "Guitarra electrica",
+      price: 28900,
+      pictureUrl:
+        "https://http2.mlstatic.com/D_NQ_NP_885128-MLA32880469711_112019-O.webp",
+    },
+  ];
 
   //Se asocia a la variable "obtenerData" una promesa que obtiene un objeto con los datos de del array de objetos en forma diferida utilizando setTimeout
-  const obtenerData = new Promise ((resolve, reject) => {
+  const obtenerData = new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(serverData)
-      reject("Hubo un error al obtener los datos del servidor")
-    }, 2000)
-  })
+      resolve(serverData);
+      reject("Hubo un error al obtener los datos del servidor");
+    }, 2000);
+  });
 
   //Se utiliza useEffect para realizar un efecto de montaje que emita un llamado asincronico a un objeto que devuelve una promesa con un conjunto de items >> si la promesa retorna fullfilled se seteara el estado "items" con lo que haya en "serverData", si retorna rejected se seteara "error" con el mensaje "Hubo un error al obtener los datos del servidor"
   //Ademas se le agrega el estado "loading" que se mostrara al hacerse true hasta que se obtengan los datos del array y vuelva a ponerse en false
 
-  useEffect( async () => {
-    setLoading(true)
+  useEffect(async () => {
+    setLoading(true);
     await obtenerData
-      .then(response => {
-        setItems(response)
+      .then((data) => {
+        setItems(data);
       })
-      .catch(err => {
-        setError(err)
-      })
-    setLoading(false)
-  }, [])
+      .catch((err) => {
+        setError(err);
+      });
+    setLoading(false);
+  }, []);
 
-
-
+  //se crea un metodo que recibe como parametro un item y setea el estado de selected item con el contenido de dicho item >> para saber que item es el seleccionado
+  const selectItem = (item) => setSelectedItem(item);
 
   return (
     <div className="container text-center">
       <h1 className="text-center py-2">{text}</h1>
 
-      {/* cuando loading este en "true" (mientras se espera la respuesta de la promesa) se renderizara el texto "CARGANDO...", pero cuando pase a ser false nuevamente (luego de recibir la respuesta diferida de la promesa) dejara de ren derizar eso y renderizara el componente "ItemList" */}
+      {/* Se muestra el mensaje de "cargando..." hasta que se recupere la informacion de la promesa y se vuelva a poner en false */}
+      {loading && (
+        <h1 style={{ color: "red", fontSize: "8rem" }}>CARGANDO...</h1>
+      )}
+
+      {/* En este renderizado condicional se mostrara >> En caso de que se selecione un item y selectedItem tenga contenido, se renderizara el ItemDetailContainer al cual se le va  apasar la informacion de dicho item para que lo renderize y en caso de que no se seleccione nada (selectedItem == null) se renderizara la lista de items */}
+      {!selectedItem ? (
+        <ItemList items={items} selectItem={selectItem} />
+      ) : (
+        <ItemDetailContainer
+          selectedItem={selectedItem}
+          selectItem={selectItem}
+          setError={setError}
+          setLoading={setLoading}
+        />
+      )}
+
+      {/* cuando loading este en "true" (mientras se espera la respuesta de la promesa) se renderizara el texto "CARGANDO...", pero cuando pase a ser false nuevamente (luego de recibir la respuesta diferida de la promesa) dejara de ren derizar eso y renderizara el componente "ItemList"
       {loading
         ? <h1 style={{color: "red", fontSize: "8rem" }}>CARGANDO...</h1>
         : <ItemList items={items} />
-      }
+      } */}
 
       {/* si error deja de ser null y pasa a contener algo se renderizara el mensaje de error que se encontrara dentro del estado error al haber sido seteado en el catch de la promesa por el fallo de la misma */}
       {error && <div>{error}</div>}
-
     </div>
   );
 };
